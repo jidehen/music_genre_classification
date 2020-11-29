38a872c6eb9d50172d57cb4c231936affc621773
Jordan Idehen <jidehen@Jordans-MBP.fios-router.home>
2020-11-29 02:33:49 -0500

    have 518,1 array of features for each track

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from preprocess import get_data
 import numpy as np
 
 def main():
-    train_inputs, train_labels = get_data('../data/fma_metadata/tracks.csv', '../data/fma_metadata/features.csv')
+    train_inputs, train_labels, test_inputs, test_labels = get_data('../data/fma_metadata/tracks.csv', '../data/fma_metadata/features.csv')
+    print("train labels: ", train_labels)
 
 if __name__ == '__main__':
     main()
--- src/preprocess.py
+++ src/preprocess.py
@@ -10,18 +10,35 @@ def get_data(track_path, feature_path):
     inputs = []
     labels = []
 
+    genres = ['Soul-RnB', 'Old-Time / Historic', 'Rock', 'Instrumental', 'Blues', 'Country', 'Electronic', 'Folk', 'Pop', 'Hip-Hop', 'Spoken', 'International', 'Classical', 'Jazz']
+    genre_dict = {genre:id for id, genre in enumerate(genres)}
+
     tracks = pandas.read_csv(track_path, index_col=0, header=[0, 1])
     tracks = tracks['track'].to_numpy()
 
     features = pandas.read_csv(feature_path, index_col=0, header=[0, 1, 2])
     features = features.to_numpy()
 
-    for i in range(len(tracks) - 1):
-        track_id = i+2
-        title = tracks[i][19]
-        genres_all = tracks[i][9]
-        feature = features[i]
-        inputs.append(Track(track_id, title, genres_all, feature))
-        labels.append(genres_all)
+    for i in range(1):
+        if tracks[i][7] in genre_dict:
+            # track_id = i+2
+            # title = tracks[i][19]
+            # genre = genre_dict[tracks[i][7]]
+            # feature = features[i]
+            inputs.append(features[i])
+            labels.append(genre_dict[tracks[i][7]])
 
-    return inputs, labels
+    rand = np.random.permutation(len(inputs))
+    inputs = inputs[rand]
+    labels = labels[rand]
+
+    train_inputs = inputs[:int((len(data)*.85))]
+    train_labels = labels[:int((len(data)*.85))]
+    test_inputs = inputs[int((len(data)*.85)):]
+    test_labels = labels[int((len(data)*.85)):]
+
+    for i in range(len(train_inputs)):
+        print("inputs val: ", train_inputs[i])
+        print("labels val: ", train_labels[i])
+
+    return train_inputs, train_labels, test_inputs, test_labels
